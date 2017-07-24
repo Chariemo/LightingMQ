363e027cf1d42a2b29d855c7c36ebe164f8eb6b6
Chariemo <charleyschen@163.com>
2017-07-24 19:24:47 +0800

    topic queue file index interface init

---
+++ src/main/java/com/rie/LightingMQ/storage/Index.java
@@ -0,0 +1,45 @@
+package com.rie.LightingMQ.storage;
+
+public interface Index {
+
+    String MAGIC = "lmqv.1.0";
+    int INDEX_SIZE = 32;
+    int READ_FILENO_OFFSET = 8;
+    int READERINDEX_OFFSET = 12;
+    int READ_CNT_OFFSET = 16;
+    int WRITE_FILENO_OFFSET = 20;
+    int WRITERINDEX_OFFSET = 24;
+    int WRITE_CNT_OFFSET = 28;
+
+    int getReadFileNo();
+
+    int getReaderIndex();
+
+    int getReadCounter();
+
+    int getWriteFileNo();
+
+    int getWriterIndex();
+
+    int getWriteCounter();
+
+    void setMagic();
+
+    void setReadFileNo(int readFileNo);
+
+    void setReaderIndex(int readerIndex);
+
+    void setReadCounter(int readable);
+
+    void setWriteFileNo(int writeFileNo);
+
+    void setWriterIndex(int writerIndex);
+
+    void setWriteCounter(int writable);
+
+    void sync();
+
+    void close();
+
+    void reset();
+}
